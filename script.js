ab826ed2dcfb672451629d1c06c48ba605de149d
per53us <73566121+Per53us@users.noreply.github.com>
2020-12-11 11:36:29 -0500

    add rng

---
+++ script.js
@@ -0,0 +1,21 @@
+/*the basic function of this .js file is to generate a random message, using rng, arrays
+of words, and string interpolation*/
+
+// Random Number Generation:
+let r = Math.floor(Math.random()*10)
+
+
+
+
+
+
+
+
+
+
+let message = ``
+/*Below is a console.log() statement I used for checking my work within this .js file by
+alternatively commenting it in and out as needed. For the actual project, I chose to run 
+the project in a web browser for more accessibility. As this is the case, the functionailty 
+of this file imported to the html file, where Document.write() is used.*/
+console.log(message)
